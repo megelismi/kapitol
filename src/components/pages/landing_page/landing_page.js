ba7ce130a53d013fd81295aad8aff00bd6e68b29
Megan Smith <meg.eli.smi@gmail.com>
2017-03-18 14:14:01 -0700

    landing_page setup to be load on default

--- src/components/pages/landing_page/landing_page.js
+++ src/components/pages/landing_page/landing_page.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'; 
-import capitol from '../images/capitol_hill.jpg'; 
-import '../styles/main.css';
+import capitol from '../../../images/capitol_hill.jpg'; 
+import '../../../styles/main.css';
 
-class App extends Component {
+class LandingPage extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Button bsStyle="primary">Primary</Button>
         <img className="capitol" alt="" src={capitol} />
       </div>
@@ -20,4 +17,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import LandingPage from './components/pages/landing_page/landing_page';
 
 ReactDOM.render(
-  <App />,
+  <LandingPage />,
   document.getElementById('root')
 );
