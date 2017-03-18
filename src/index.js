fd3a39f2be68d7451e9260adcf56768024f68b58
Megan Smith <meg.eli.smi@gmail.com>
2017-03-18 13:31:58 -0700

    files organized in folders

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
